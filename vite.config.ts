00ec9530b0b7f6b1c9ac6d023e962f0901e38a19
Israel Silva <zrael002@gmail.com>
2026-04-20 16:24:00 -0300

    fix: base path for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/TaskFlow/", // 👈 ADICIONE ISSO AQUI
+
   server: {
     host: "::",
     port: 8080,
@@ -11,11 +13,20 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
+
   plugins: [react()],
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-    dedupe: ["react", "react-dom", "react/jsx-runtime", "react/jsx-dev-runtime", "@tanstack/react-query", "@tanstack/query-core"],
+    dedupe: [
+      "react",
+      "react-dom",
+      "react/jsx-runtime",
+      "react/jsx-dev-runtime",
+      "@tanstack/react-query",
+      "@tanstack/query-core",
+    ],
   },
 }));
--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: "/TaskFlow/", // 👈 ESSENCIAL PARA GITHUB PAGES
+  base: "/TaskFlow/", // 
 
   plugins: [react()],
 
